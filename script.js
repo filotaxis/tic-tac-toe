fff9e8e14c5445739a09b7669448e5f00b1eac4e
Bryan <bryan.anidem@gmail.com>
2021-10-24 18:57:05 -0500

    added a tie case, and better UI

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const Gameboard = (() => {
     const gridSpace = document.querySelector(".gameboard");
-    let gameboard = new Array(9);
+    const announce = document.querySelector("h1");
+    let gameboard = ["", "", "", "", "", "", "", "", ""];
     const winningConditions = [
         [0, 1, 2],
         [3, 4, 5],
@@ -21,7 +22,7 @@ const Gameboard = (() => {
                 if (i === winnerCombination[0] ||
                     i === winnerCombination[1] ||
                     i === winnerCombination[2]) {
-                    tile.style.backgroundColor = "tomato";
+                    tile.style.backgroundColor = "#f7fff7";
                     }
             }
             gridSpace.appendChild(tile);
@@ -35,18 +36,17 @@ const Gameboard = (() => {
     }
 
     const winner = () => {
-        let = false;
         for (let i = 0; i < winningConditions.length; i++){
             const winCondition = winningConditions[i];
             let a = gameboard[winCondition[0]];
             let b = gameboard[winCondition[1]];
             let c = gameboard[winCondition[2]];
 
-            if (a === b && a === c && a !== undefined) {
-                won = true;
-                console.log(`${a} won`);
+            if (a === b && a === c && a !== "") {
+                announce.textContent = `🙌 ${a} won 🏆️ 🙌`;
+
                 for (let i = 0; i < 9; i++){
-                    if (gameboard[i] === undefined) {
+                    if (gameboard[i] === "") {
                         gameboard[i] = null;
                     }
                 }
@@ -58,6 +58,7 @@ const Gameboard = (() => {
     return {
         gameboard,
         gridSpace,
+        announce,
         display,
         resetDisplay,
         winner,
@@ -67,7 +68,7 @@ const Gameboard = (() => {
 
 const Player = (emoji) => {
     const icon = emoji ;
-
+    
     return {
         icon,
     }
@@ -75,23 +76,32 @@ const Player = (emoji) => {
 
 const Game = (() => {
 
+    const resetButton = document.querySelector(".restart>button");
     let turn = true;
+    let turnCount = 0;
 
     const play = (player1, player2) => {
         Gameboard.gridSpace.addEventListener("click", (e) => {
             const id = e.target.getAttribute("data-id");
-            if (Gameboard.gameboard[id] === undefined) {
+            if (Gameboard.gameboard[id] === "") {
                 if (turn === true) {
                     Gameboard.gameboard[id] = player1.icon;
+                    Gameboard.announce.textContent = `${player2.icon}'s turn`
+                    turnCount++;
                     turn = !turn;
                 }
                 else {
                     Gameboard.gameboard[id] = player2.icon;
+                    Gameboard.announce.textContent = `${player1.icon}'s turn`
                     turn = !turn;
+                    turnCount++;
                 }
 
                 const winnerCombination = Gameboard.winner();
-                // console.log(winnerCombination);
+
+                if (turnCount === 9 && !winnerCombination) {
+                    Gameboard.announce.textContent = "It's a tie 🐱♥️🐶"
+                }
                 Gameboard.resetDisplay();
                 Gameboard.display(winnerCombination);
             }
@@ -99,16 +109,40 @@ const Game = (() => {
 
     }
 
+    const resetGame = () => {
+        resetButton.addEventListener("click", () => {
+            Gameboard.announce.textContent = "🐱 Tic-Tac-Toe 🐶";
+            for (let i = 0; i < 9; i++){
+                Gameboard.gameboard[i] = "";
+            }
+            turnCount = 0;
+            Gameboard.resetDisplay();
+            Gameboard.display();
+        })
+    }
+
     return {
         play,
+        resetGame,
     }
 
 })();
 
-const player1 = Player("🐱");
-const player2 = Player("🐶");
-Game.play(player1, player2);
-Gameboard.display();
+const main = (() => {
+    const play = () => {
+        const player1 = Player("🐱");
+        const player2 = Player("🐶");
+        Game.play(player1, player2);
+        Gameboard.display();
+        Game.resetGame();
+    }
+    return {
+        play,
+    }
+})();
+
+main.play();
+
 
 
 
